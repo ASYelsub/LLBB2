caa85028acae8a80c8b25756753cafa42c5b9410
Heyward_Geneva <geneva@glowup.games>
2020-05-19 19:23:08 -0400

    from wheni initially started working on the new changes. about to merge with the new stuff in master

--- Assets/Scripts/Prototype/TextHolder.cs
+++ Assets/Scripts/Prototype/TextHolder.cs
@@ -10,40 +10,29 @@ public class TextHolder : MonoBehaviour
 {
     [SerializeField] private string[] dialogue = new string[20];
     public GameManager gameManager;
-    public bool isColliding;
     [SerializeField] private MeshRenderer notifCube;
 
     private void Start()
     {
-        isColliding = false;
         notifCube.enabled = false;
     }
 
     private void Update()
     {
-        if (Input.GetKey(KeyCode.Return) || Input.GetKey(KeyCode.Space))
-        {
-            if (isColliding)
-            {
-                gameManager.DisplayDialogue(dialogue[0]);
-                notifCube.enabled = true;
-            }
-            else
-            {
-                notifCube.enabled = false;
-            }
-        }
-        //print(isColliding);
+
     }
 
-    private void OnTriggerEnter(Collider other)
+    private void OnTriggerStay(Collider other)
     {
-        isColliding = true;
+        if (Input.GetKey(KeyCode.Return) || Input.GetKey(KeyCode.Space))
+        {
+            gameManager.DisplayDialogue(dialogue[0]);
+            notifCube.enabled = true;
+        }
     }
 
     private void OnTriggerExit(Collider other)
     {
-        isColliding = false;
         notifCube.enabled = false;
         gameManager.TurnOffDialogue();
     }
---
+++ Assets/Scripts/Year.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Year
+{
+    public Month[] months;
+
+    public class Time
+    {
+
+    }
+
+    public class Day
+    {
+        public Time[] timeOfDay;
+    }
+
+    public class Week
+    {
+        public Day[] days;   
+    }
+
+    public class Month
+    {
+        public Week[] weeks;
+        public string _name;
+
+        public Month(string s)
+        {
+            s = _name;
+        }
+    }
+   
+}
