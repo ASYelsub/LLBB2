be7eb133d1385448a07ddb8edc071bead89370e2
Heyward_Geneva <geneva@glowup.games>
2020-05-19 21:44:24 -0400

    Edited the Character and Year scripts to try and account for different things we may need. Tomorrow I will: - fix these scripts a bit to clean it up and add comments - Refactor how location movement is handled - Create character scriptable objects - Make sure dialogue pops up when a button is pressed/the player is near them

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -2,17 +2,36 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Character : MonoBehaviour
+[CreateAssetMenu(fileName ="Objects",menuName ="Characters",order = 0)]
+public class Character : ScriptableObject
 {
-    // Start is called before the first frame update
-    void Start()
+
+    public string _name;
+
+    //STATS
+    public int whamitude, popularity, school, spirit, 
+        creepiness, skinThickness, independence, 
+        empathy, strut, health, stamina;
+
+    public int closenessToPlayer;
+
+    public Year.Schedule schedule;
+
+    public Year.location myLocation;
+
+    public Sprite[] characterSprites; //their portraits;
+
+    public GameObject characterOBJ;
+
+    public void ScheduleChanged() // anytime a schedule is changed characters should move to the correct spot based on the current time
     {
-        
+        MovetoPos( Year.locations[schedule.daysInSchedule[(int)Year.currentDay].timeSlots[(int)Year.currentTime].myPos]);
+        // the above looks really ugly and complex for no reason :/ need to fix tomorrow
     }
 
-    // Update is called once per frame
-    void Update()
+    public void MovetoPos(Vector3 pos)
     {
-        
+        characterOBJ.transform.position = pos;
     }
+
 }
--- Assets/Scripts/Year.cs
+++ Assets/Scripts/Year.cs
@@ -2,33 +2,72 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Year
+[System.Serializable]
+public static class Year
 {
-    public Month[] months;
+    public static Month[] months;
+    public static int currentYear, currentMonth;
+    public static Day currentDay;
+    public static Time currentTime;
 
-    public class Time
+    public static Vector3[] locations;
+    public enum location
     {
-
+        LinguaLounge = 0, ScienceLab = 1, TeachersOffice = 2,
+        ComputationRoom = 3, WeightRoom = 4, FootballField = 5,
+        DarkRoom = 6, TheShed = 7, CostumeCloset = 8,
+        PublicTheater = 9, WritingDen = 10, BackField = 11,
+        RadioTower = 12, River = 13, SecretGarden = 14,
+        BoxingRing = 15, PostOffice = 16, Diner = 17,
+        ExtraTerrestrial = 18, Dormitory = 19, WateredMelon = 20,
+        TheQuad = 21, TheTrainer = 22, Chapel = 23, ChemicalBasement = 24
     }
 
-    public class Day
+    public static string GetDateAsString()
     {
-        public Time[] timeOfDay;
+        return currentDay + ", " + months[currentMonth].GetCurrentMonth() + " " + months[currentMonth].dayInMonth + ", " + currentYear;
     }
 
-    public class Week
+    public static int GetDateAsInt()
     {
-        public Day[] days;   
+        string s = currentMonth + months[currentMonth].dayInMonth.ToString() + currentYear;
+        return int.Parse(s);
     }
 
+  
+
+    public class Schedule
+    {
+        public DayInSchedule[] daysInSchedule; // should be 7
+    }
+
+    public class TimeSlot
+    {
+        public Time myTime;
+        public int myLocation, myPos; // ints are used to find the location and locationPos
+    }
+
+    [System.Serializable]
+    public class DayInSchedule
+    {
+        public Day myDay;
+        public TimeSlot[] timeSlots;
+    }
+
+    public enum Time { Morning, Class, Afternoon, Evening };
+    public enum Day { Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday };
+    public enum WhichMonth {  January = 1, February, March, April, May, June, July, August, September, October, November, December};
+
+    [System.Serializable]
     public class Month
     {
-        public Week[] weeks;
-        public string _name;
+        public int numOfWeeks; // how many weeks are in this month:
+        public WhichMonth month;
+        public int dayInMonth; //ranging from 1-31
 
-        public Month(string s)
+        public string GetCurrentMonth()
         {
-            s = _name;
+            return month.ToString();
         }
     }
    
